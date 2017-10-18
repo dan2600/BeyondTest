8aed62cbcf9c32aeb119cc13ab2290eb1db6bff2
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-10-18 16:36:54 -0400

    clean up files

--- public/controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('routerApp', ['ui.router']);.controller('listController', ['$scope', '$http', '$state', '$location', function($scope, $http, $state, $location) {
-    $http.get('/api/playlist').success(function(data){
-      $scope.thisAlbum = $state.params.id;
-      $scope.albums = data;
-    });
-}]);
