f5da2216cb24f38f5f79f1d3e5a14a464dec5c65
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-10-18 16:41:36 -0400

    final code cleanup

--- index.js
+++ index.js
@@ -10,33 +10,18 @@ var path = require('path');
 //Data URL for JSON
 var url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw';
 
-app.use(
-    sass({
-        src: __dirname + '/sass',
-        dest: __dirname + '/public',
-        debug: true,
-    })
-);
-
-
-
-
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 var port = process.env.PORT || 8080;
 
-
-
 // routes
-
 
 //we Could just access the playlist from the frontend but might as well build a custom API endpoint. 
 app.get('/api/playlist', function(req, res) {
-
     //This would be built as an actual API call. 
     https.get(url, function(r2) {
         var body = '';
@@ -52,14 +37,10 @@ app.get('/api/playlist', function(req, res) {
     }).on('error', function(e) {
         console.log("Got an error: ", e);
     });
-
     //Load the fontend
     app.get('*', function(req, res) {
         res.sendfile('./public/index.html');
     });
 });
-
-
-
 app.listen(port);
 console.log("App listening on port 8080");
